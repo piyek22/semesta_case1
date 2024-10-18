2870b4ae857fe6e08e6bc21e16b99bcf0c5fa6b5
Muhammad Rafli Rizqi <rrizqi114@gmail.com>
2024-10-18 07:56:32 +0700

    lupa

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,17 @@
 <?php
 
-use App\Http\Controllers\ObatController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ObatController;
 
-// Rute default untuk halaman utama
-Route::get('/', [ObatController::class, 'loadAllObats']);
+// Rute default
+Route::get('/', function () {
+    return redirect('/obats'); // Mengarahkan ke halaman daftar obat
+});
 
-// Menampilkan semua obat
+// Rute lainnya
 Route::get('/obats', [ObatController::class, 'loadAllObats']);
-
-// Menampilkan form untuk menambahkan obat
 Route::get('/add/obat', [ObatController::class, 'loadAllObatsForm']);
-
-// Proses penambahan obat baru
 Route::post('/add/obat', [ObatController::class, 'AddObat'])->name('AddObat');
-
-// Menampilkan form edit untuk obat tertentu
 Route::get('/edit/{id}', [ObatController::class, 'loadEditForm']);
-
-// Proses pengeditan obat
 Route::post('/edit/obat', [ObatController::class, 'EditObat'])->name('EditObat');
-
-// Menghapus obat
 Route::get('/delete/{id}', [ObatController::class, 'deleteObat'])->name('DeleteObat');
